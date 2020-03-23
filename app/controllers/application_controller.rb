78f7e4486d88dcfdde350a7c8aad1f2e2db8af33
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-23 03:31:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,20 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do 
+    erb :super_hero
+  end 
+
+  post '/teamer' do
+  
+    @team = Team.new(params[:team])
+    @hero_1 = Hero.new(name: params[:member1_name], power: params[:member1_power], bio: params[:member1_bio])
+    @hero_2 = Hero.new(name: params[:member2_name], power: params[:member2_power], bio: params[:member2_bio])
+    @hero_3 = Hero.new(name: params[:member3_name], power: params[:member3_power], bio: params[:member3_bio])
+
+
+    @heroes = Hero.all 
+    erb :team
+  end 
 
 end
